4b1279d48b05c569dc7f332f9d5f5048397dfaaa
Bob Starr <rtzaudio@comcast.net>
2021-09-23 20:19:56 -0400

    Changed stat command

--- CLITask.c
+++ CLITask.c
@@ -706,36 +706,42 @@ void cmd_stat(int argc, char *argv[])
 {
     /* Show basic system status */
     CLI_printf("\nSystem Status\n\n");
-    CLI_printf("  Tape roller tach   : %u\n", (uint32_t)g_sys.tapeTach);
-    CLI_printf("  Tape roller errors : %u\n", g_sys.qei_error_cnt);
-    CLI_printf("  Encoder position   : %d\n", g_sys.tapePosition);
-    CLI_printf("  Tape Speed         : %d IPS\n", g_sys.tapeSpeed);
-    CLI_printf("  RTC clock type     : %s\n", (g_sys.rtcFound) ? "RTC" : "CPU");
+    CLI_printf("Tape roller tach   : %u\n", (uint32_t)g_sys.tapeTach);
+    CLI_printf("Tape roller errors : %u\n", g_sys.qei_error_cnt);
+    CLI_printf("Encoder position   : %d\n", g_sys.tapePosition);
+    CLI_printf("Tape Speed         : %d IPS\n", g_sys.tapeSpeed);
+    CLI_printf("RTC clock type     : %s\n", (g_sys.rtcFound) ? "RTC" : "CPU");
+    CLI_printf("IPC rx errors      : %d\n", g_ipc.rxErrors);
 
     /* Show if DCS controller found or not */
-    CLI_printf("  DCS controller     : ");
+    CLI_printf("DCS controller     : ");
     if (g_sys.dcsFound)
         CLI_printf("%d track\n", g_sys.trackCount);
     else
         CLI_printf("(n/a)\n");
 
+    /* Show standby monitor state */
+    CLI_printf("Standby Mon Active : %c\n", (g_sys.standbyActive) ? '1' : '0');
+
     /* Show if SMPT controller found or not */
-    CLI_printf("  SMPTE controller   : ");
+    CLI_printf("SMPTE controller   : ");
     if (g_sys.smpteFound)
         CLI_printf("found\n");
     else
         CLI_printf("(n/a)\n");
 
+#if 0
     /* Show IPC Server Status */
     CLI_printf("\nIPC Server\n");
-    CLI_printf("  Rx Errors      : %d\n", g_ipc.rxErrors);
-    CLI_printf("  Rx Count       : %d\n", g_ipc.rxCount);
-    CLI_printf("  Rx Num Free    : %d\n", g_ipc.rxNumFreeMsgs);
-    CLI_printf("  Rx Last Seq    : %d\n", g_ipc.rxLastSeq);
-    CLI_printf("  Rx ExpectedSeq : %d\n", g_ipc.rxExpectedSeq);
-    CLI_printf("  Tx Count       : %d\n", g_ipc.txCount);
-    CLI_printf("  Tx Num Free    : %d\n", g_ipc.txNumFreeMsgs);
-    CLI_printf("  Tx Next Seq    : %d\n", g_ipc.txNextSeq);
+    CLI_printf("Rx Errors      : %d\n", g_ipc.rxErrors);
+    CLI_printf("Rx Count       : %d\n", g_ipc.rxCount);
+    CLI_printf("Rx Num Free    : %d\n", g_ipc.rxNumFreeMsgs);
+    CLI_printf("Rx Last Seq    : %d\n", g_ipc.rxLastSeq);
+    CLI_printf("Rx ExpectedSeq : %d\n", g_ipc.rxExpectedSeq);
+    CLI_printf("Tx Count       : %d\n", g_ipc.txCount);
+    CLI_printf("Tx Num Free    : %d\n", g_ipc.txNumFreeMsgs);
+    CLI_printf("Tx Next Seq    : %d\n", g_ipc.txNextSeq);
+#endif
 }
 
 void cmd_cfg(int argc, char *argv[])
