174698efd8a20a4d60c632d2608c017070167fda
Bob Starr <rtzaudio@comcast.net>
2024-02-25 18:21:49 -0500

    added font

--- fonts/makefont.bat
+++ fonts/makefont.bat
@@ -6,4 +6,4 @@ ftrasterize -r -m -f DSEG7Bold -s 16 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
 ftrasterize -r -m -f DSEG7Bold -s 18 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
 ftrasterize -r -m -f DSEG7Bold -s 20 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
 ftrasterize -r -m -f DSEG7Bold -s 24 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
-ftrasterize -r -m -f MonoSpace -s 6 monospace-regular.ttf -p 48 -e 58
+ftrasterize -r -m -f MonoSpace -s 6 monospace-regular.ttf
