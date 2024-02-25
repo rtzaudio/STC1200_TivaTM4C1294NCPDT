023a24ba2f5ff1356aac59f68e3258286b22d56f
Bob Starr <rtzaudio@comcast.net>
2024-02-25 18:13:48 -0500

    add new font

--- fonts/makefont.bat
+++ fonts/makefont.bat
@@ -1,3 +1,4 @@
+set PATH=%PATH%;C:\ti\TivaWare_C_Series-2.2.0.295\tools\bin
 ftrasterize -r -m -f DSEG7Bold -s 10 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
 ftrasterize -r -m -f DSEG7Bold -s 12 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
 ftrasterize -r -m -f DSEG7Bold -s 14 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
@@ -5,3 +6,4 @@ ftrasterize -r -m -f DSEG7Bold -s 16 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
 ftrasterize -r -m -f DSEG7Bold -s 18 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
 ftrasterize -r -m -f DSEG7Bold -s 20 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
 ftrasterize -r -m -f DSEG7Bold -s 24 DSEG7ClassicMini-Bold.ttf -p 48 -e 58
+ftrasterize -r -m -f MonoSpace -s 6 monospace-regular.ttf -p 48 -e 58
